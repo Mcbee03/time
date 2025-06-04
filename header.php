676d8d661379c4b88d4f89544e803312100f683b
Mcbee03 <cudalkeithharvey@gmail.com>
2025-06-04 15:09:58 +0800

    updateuli

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DATE RATE TIME. | <?= $pageTitle ?? 'Admin' ?></title>
+    <title>DATE RATE TIME | <?= $pageTitle ?? 'Admin' ?></title>
 
     <!-- Google Font - Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
